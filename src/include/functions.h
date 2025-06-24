12b3beac6062e5dc20bb417492b07032f12d7afb
Александр Сергиенко <s_sandr06@mail.ru>
2025-06-24 22:44:26 +0300

    Создан файл functions.h

---
+++ src/include/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+// Функции из условия задачи
+extern double f1(double x);
+extern double f2(double x);
+extern double f3(double x);
+
+#endif //FUNCTIONS_H
